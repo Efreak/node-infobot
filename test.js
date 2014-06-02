d8fd0727bd76b695d791a1875f73ac1984f1c5aa
Efreak <Efreak2004@gmail.com>
2014-06-01 17:38:02 -0700

    Add infobot.db, because I'm too lazy/nub to deal with properly adding things to asynchronous sqlite3

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ var bot = new InfoBot('infobot.db');
 
 console.log('getinfo');
 console.log(bot.getInfo('word'));
-console.log('setdefinition');
-console.log(bot.addWord('word','word definition','steamid','steamname'));
+//console.log('setdefinition');
+//console.log(bot.addWord('word','word definition','steamid','steamname'));
 console.log('getword');
 console.log(bot.getDefinition('word'));
 console.log('getinfo');
